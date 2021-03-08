16305cd5774e0e4f2c8cd4529ed020cf2b81bdd4
Sebastian <46867564+seba0456@users.noreply.github.com>
2021-03-08 17:42:44 +0100

    Update NBS.cpp

--- NBS.cpp
+++ NBS.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 #include <cstdlib>
+#include <limits>
 using namespace std;
-int main()
+int ins_error(int minimum_operation_number, int  maximum_operation_number, int instruction)
+{
+	while (instruction < minimum_operation_number || instruction > maximum_operation_number)
+	{
+		if (!cin) // or if(cin.fail())
+		{
+			// user didn't input a number
+			cin.clear(); // reset failbit
+			cin.ignore(numeric_limits<std::streamsize>::max(), '\n'); //skip bad input
+			// next, request user reinput
+		}
+		else
+		{
+			cout << "Invaild instruction, please try again..." << endl;
+			cin >> instruction;
+		}
+
+	}
+	return (instruction);
+}
+int nbs()
 {
 	int day;
 	int start;
@@ -37,7 +58,7 @@ int main()
 	cotb = 500;
 	hunger = 3000;
 	thirst = 150;
-	hp = 100;	
+	hp = 100;
 	int instruction;
 	instruction = 6;
 	string note;
@@ -45,7 +66,7 @@ int main()
 	int sl;
 	sl = 1;
 	ex = 0;
-	while ( sl == 1)
+	while (sl == 1)
 	{
 		if (hp <= 0 || cotb <= 0)
 		{
@@ -173,116 +194,116 @@ int main()
 						{
 							note = "Home, sweet home!";
 						}
-					}				
+					}
 					else
-				note = "Why should I repair my shelter? Everything is fine!";
+						note = "Why should I repair my shelter? Everything is fine!";
 				}
 				else
 					note = "I have no resources unneccesarty for repairs!";
 			}
 			else if (instruction == 3)
 			{
-			int local_hp;
-			if (medkit > 0)
-			{
-				if (hp < 80)
+				int local_hp;
+				if (medkit > 0)
 				{
-					int rr_event;
-					rr_event = (rand() % 3) + 1;
-					hp = hp + 40;
-					medkit = medkit - 1;
-					if (rr_event == 1)
-						note = "I feel much better!";
-					else if (rr_event == 2)
-						note = "My wound doesn't look good, but I'm hopeful!";
-					else if (rr_event == 3)
+					if (hp < 80)
 					{
-						note = "It's hurts less";
+						int rr_event;
+						rr_event = (rand() % 3) + 1;
+						hp = hp + 40;
+						medkit = medkit - 1;
+						if (rr_event == 1)
+							note = "I feel much better!";
+						else if (rr_event == 2)
+							note = "My wound doesn't look good, but I'm hopeful!";
+						else if (rr_event == 3)
+						{
+							note = "It's hurts less";
+						}
 					}
+					else
+						note = "So much better!";
 				}
 				else
-					note = "So much better!";
-			}
-			else
-				note = "I have no medkits!";
+					note = "I have no medkits!";
 			}
 			else if (instruction == 4)
 			{
-			
-			if (water > 0)
-			{
-				if (thirst < 80)
+
+				if (water > 0)
 				{
-					int rr_event;
-					rr_event = (rand() % 3) + 1;
-					thirst = thirst + 150;
-					water = water - 1;
-					if (rr_event == 1)
-						note = "I feel much better!";
-					else if (rr_event == 2)
-						note = "It's better than a water from nearest river!";
-					else if (rr_event == 3)
+					if (thirst < 80)
 					{
-						note = "Finaly watter!";
+						int rr_event;
+						rr_event = (rand() % 3) + 1;
+						thirst = thirst + 150;
+						water = water - 1;
+						if (rr_event == 1)
+							note = "I feel much better!";
+						else if (rr_event == 2)
+							note = "It's better than a water from nearest river!";
+						else if (rr_event == 3)
+						{
+							note = "Finaly watter!";
+						}
 					}
+					else
+						note = "I'm fine!";
 				}
 				else
-					note = "I'm fine!";
-			}
-			else
-				note = "I have no medkits!";
+					note = "I have no medkits!";
 			}
 			else if (instruction == 5)
 			{
-			int local_hunger;
-			if (food > 0)
-			{
-				if (hunger < 2500)
+				int local_hunger;
+				if (food > 0)
 				{
-					int rr_event;
-					rr_event = (rand() % 3) + 1;
-					hunger = hunger + 500;
-					food = food - 1;
-					if (rr_event == 1)
-						note = "I feel much better!";
-					else if (rr_event == 2)
-						note = "This food is only 10 yeras old! It's tasty!";
-					else if (rr_event == 3)
+					if (hunger < 2500)
 					{
-						note = "I like this ham!";
+						int rr_event;
+						rr_event = (rand() % 3) + 1;
+						hunger = hunger + 500;
+						food = food - 1;
+						if (rr_event == 1)
+							note = "I feel much better!";
+						else if (rr_event == 2)
+							note = "This food is only 10 yeras old! It's tasty!";
+						else if (rr_event == 3)
+						{
+							note = "I like this ham!";
+						}
 					}
+					else
+						note = "I'm not hungry!";
 				}
 				else
-					note = "I'm not hungry!";
-			}
-			else
-				note = "I have no food!";
+					note = "I have no food!";
 			}
 			else if (instruction == 6)
 			{
-			int stay_e;
-			stay_e = (rand() % 5) + 1;
-			if (stay_e == 1)
-			{
-				note = "It was quiet here, but I'm save here.";
-			}
-			else if (stay_e == 2)
-			{
-				note = "Raiders attacked my shelter!";
-				cotb = cotb - 200;
-			}
-			else if (stay_e == 3)
-			{
-				note = "I ready to go!";
-			}
-			else if (stay_e == 4)
-			{
-				note = "I have played chess - with myself...";
-			}
-			else if (stay_e == 5)
-			{
-				note = "I'm wondering, why world ended";
-			}
+				int stay_e;
+				stay_e = (rand() % 5) + 1;
+				if (stay_e == 1)
+				{
+					note = "It was quiet here, but I'm save here.";
+				}
+				else if (stay_e == 2)
+				{
+					note = "Raiders attacked my shelter!";
+					cotb = cotb - 200;
+				}
+				else if (stay_e == 3)
+				{
+					note = "I ready to go!";
+				}
+				else if (stay_e == 4)
+				{
+					note = "I have played chess - with myself...";
+				}
+				else if (stay_e == 5)
+				{
+					note = "I'm wondering, why world ended";
+				}
 			}
 			else if (instruction == 7)
 			{
@@ -292,6 +313,7 @@ int main()
 			{
 				// lol nothing!
 			}
+			system("Color 0A");
 			cout << "     Personal termianl (r) 1989     " << endl;
 			cout << "      ____________     " << endl;
 			cout << "     /            |    " << endl;
@@ -330,42 +352,58 @@ int main()
 			cout << "4 - Drink water" << endl;
 			cout << "5 - Eat food" << endl;
 			cout << "6 - Stay in shelter" << endl;
-			cout << "7 - Exit game" << endl;
-
+			cout << "7 - Exit to Windows" << endl;
 			cout << "____________________________________________________________________" << endl;
 			cout << "Your action: ";
 			cin >> instruction;
-			while (instruction < 1 || instruction > 7)
-			{
-				if (!cin) // or if(cin.fail())
-				{
-					// user didn't input a number
-					cin.clear(); // reset failbit
-					cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //skip bad input
-					// next, request user reinput
-				}
-				else
-				{
-					cout << "Invaild instruction, please try again..." << endl;
-					cin >> instruction;
-				}
-
-			}
+			ins_error(1, 7, instruction);
 
 		}
-		
+
 	}
 	cout << "Game Over!" << endl;
-	if ( hp <= 0 )
-	cout << "You died!" << endl;
+	if (hp <= 0)
+		cout << "You died!" << endl;
 	else if (cotb <= 0)
-	cout << "Your bunker has been destroyed!" << endl;
+		cout << "Your bunker has been destroyed!" << endl;
 	cout << "You have survived " << day << " days!" << endl;
 	cout << "" << endl;
 	cout << "Last known note: " << note << endl;
 	cout << "Press Enter, to exit..." << endl;
 	cin.ignore();
 	cin.get();
-		
-	
+
+
+}
+void menu()
+{
+	int instruction;
+	int instructions = 2;
+	cout << "Welcome to Seba0456's console games collection.\n";
+	cout << "All my games created here are made in C++ & and Visual Studio 2019.\n";
+	cout << "All my games created in C++ are source-open.\n";
+	cout << "Feel free to copy or modify games.\n";
+	cout << "Select your game: \n";
+	cout << "1.Nuclear Bunker Symulator\n";
+	cout << instructions << ".Exit program\n";
+	cin >> instruction;
+	instruction = ins_error(1, 2, instruction);
+	switch (instruction)
+	{
+	case 1:
+	{
+		system("cls");
+		nbs();
+	}
+	case 2:
+	{
+		exit(0);
+	}
+	}
+}
+
+
+int main()
+{
+	menu();
 }
